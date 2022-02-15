a2967caa55453ce2e225ada16ba097dcd37486a8
koikera <matheuscomelli@hotmail.com>
2022-02-15 09:16:03 -0300

    Colocando banco de dados

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,43 +13,24 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-
-        public IEnumerable<Evento> _evento =  new Evento[]{
-                new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote = "1 Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemUrl = "Foto.png"
-            }, 
-            new Evento(){
-                EventoId = 2,
-                Tema = "Angular 11 e Suas novidades",
-                Local = "São Paulo",
-                Lote = "2 Lote",
-                QtdPessoas = 370,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemUrl = "Foto1.png"
-            }
-            };
-        public EventoController()
+        public DataContext Context { get; }
+        public EventoController(DataContext context)
         {
+            this.Context = context;
 
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;        
+            return this.Context.Eventos;
         }
 
         [HttpGet("{id}")]
 
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);        
+            return this.Context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ProEventos.API.Data;
 
 namespace ProEventos.API
 {
@@ -20,12 +22,13 @@ namespace ProEventos.API
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
+                public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var sqlConnectionString = Configuration.GetConnectionString("Default");
+            services.AddDbContext<DataContext>(context => context.UseMySql(sqlConnectionString, ServerVersion.AutoDetect(sqlConnectionString)));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
